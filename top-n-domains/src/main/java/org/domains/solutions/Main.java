24f5fba4e144b17982a374f40ed02b8615f01df6
Serhii Zemlianyi <serhiizem@gmail.com>
2024-01-31 14:07:10 +0200

    read emails from stdin

--- top-n-domains/src/main/java/org/domains/solutions/Main.java
+++ top-n-domains/src/main/java/org/domains/solutions/Main.java
@@ -1,11 +1,31 @@
 package org.domains.solutions;
 
 import lombok.extern.slf4j.Slf4j;
+import org.domains.solutions.service.DomainStatisticsService;
+
+import java.io.BufferedInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 @Slf4j
 public class Main {
 
-	public static void main(String[] args) {
+	public static final int DEFAULT_TOP_DOMAINS_LIMIT = 10;
 
+	public static void main(String[] args) {
+		var stdin = new Scanner(new BufferedInputStream(System.in));
+		System.out.println("Waiting for emails input");
+
+		List<String> inputEmails = new ArrayList<>();
+		while (stdin.hasNextLine()) {
+			String email = stdin.nextLine();
+			System.out.println("Entered email: " + email);
+			inputEmails.add(email);
+		}
+
+		var domainStatisticsService = new DomainStatisticsService();
+		var topFrequentDomains = domainStatisticsService.findTopFrequentDomains(inputEmails, DEFAULT_TOP_DOMAINS_LIMIT);
+		System.out.println(topFrequentDomains);
 	}
 }
